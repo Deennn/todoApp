38de14a0ee4e46d6a7fdcfad9bbadcb00a8e3d2a
User <user@Users-MacBook-Air.local>
2022-04-14 04:17:23 +0100

    made some meaningful commit

--- src/main/java/com/deenn/demo/controller/TaskController.java
+++ src/main/java/com/deenn/demo/controller/TaskController.java
@@ -1,32 +1,40 @@
 package com.deenn.demo.controller;
 
 import com.deenn.demo.dto.TaskDto;
+import com.deenn.demo.dtos.SearchDto;
 import com.deenn.demo.model.Task;
+import com.deenn.demo.services.TaskService;
 import com.deenn.demo.services.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class TaskController {
     private final TaskServiceImpl taskService;
 
+
     @Autowired
     public TaskController(TaskServiceImpl taskService) {
         this.taskService = taskService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/tasks")
     public String home(Model model) {
         TaskDto task = new TaskDto();
+        SearchDto searchDto = new SearchDto();
+        var taskList = taskService.getAllTasks();
+        model.addAttribute("list",taskList);
         model.addAttribute("taskObject", task);
-        return "create_task" ;
+        model.addAttribute("searchDto", searchDto);
+        return "tasks";
     }
 
 
@@ -37,7 +45,7 @@ public class TaskController {
 
 
 
-    @PostMapping("/task")
+    @PostMapping("/tasks")
     public String createTask(@ModelAttribute("taskObject") TaskDto task) {
 
         try {
@@ -48,7 +56,82 @@ public class TaskController {
         return "task_creation_success";
     }
 
+//    @RequestMapping("/tasks")
+//    @ResponseBody
+//    public List<Task> allTask() {
+//        return taskService.getAllTasks();
+//
+//    }
+//    @RequestMapping("/tasks/{title}")
+//    @ResponseBody
+//    public Task taskByTitle(@PathVariable String title) {
+//        return taskService.findTaskByTitle(title);
+//    }
 
+    @GetMapping("/tasks/{title}")
+//    @ResponseBody
+    public String taskByTitle(@ModelAttribute("searchDto") SearchDto searchDto, Model model) {
+        TaskDto task = new TaskDto();
+//        task.setTitle(title);
+       Task searchTask = taskService.findTaskByTitle(searchDto.getTitle());
+//       return searchTask;
+//        List<Task> taskList = new ArrayList<>();
+//        taskList.add(searchTask);
+//        model.addAttribute("list",taskList);
+
+        model.addAttribute("searchFor",searchTask);
+        model.addAttribute("taskObject", task);
+       return "search_task";
+//        return searchTask;
+    }
+
+
+
+//    @RequestMapping(value = "/tasks/{id}", method = RequestMethod.DELETE)
+//    public String deleteTask(@PathVariable Long id) {
+//        taskService.deleteTask(id);
+//        return "redirect:/task_deleted";
+//    }
+
+//    @GetMapping("/tasks/{id}")
+//    public String edit(@PathVariable long id, Model model) {
+//        TaskDto editTask = taskService.getTaskById(id);
+//        model.addAttribute("editTask", editTask);
+//        return "tasks";
+//    }
+//    @PostMapping("/tasks")
+//    public String edit (@PathVariable Task task, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+////    public String edit (@ModelAttribute("editTask") TaskDto task) {
+//        TaskDto currentTask = taskService.getTaskById(task.getId());
+//        if (bindingResult.hasErrors()){
+//            model.addAttribute("taskTitle", currentTask.getTitle());
+//            return "tasks";
+//        }
+//        redirectAttributes.addFlashAttribute("message", "Task created!");
+//        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+//        TaskDto taskCheck = taskService.getTaskById(task.getId());
+//        if (taskCheck != null){
+//            redirectAttributes.addFlashAttribute("message", "This task already exist.");
+//            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+//            redirectAttributes.addFlashAttribute("task", task);
+//        } else {
+//            taskService.editTask( task.getTitle(), task.getDescription());
+//        }
+//        return "user/task/edit" + task.getId();     // Redirect this to task page
+//    }
+//    @GetMapping
+//    public String delete (@PathVariable long id, RedirectAttributes redirectAttributes){
+//        taskRepo.deleteById(id);
+//        redirectAttributes.addFlashAttribute("message", "Task deleted!");
+//        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+//        return "redirect:/user/task";
+//    }
+@GetMapping("/tasks/")
+public String editStudentsForm( Model model) {
+//    model.addAttribute("task", taskService.getTaskById(id));
+    return "edit_task";
+
+}
 
 
 }
---
+++ src/main/java/com/deenn/demo/dtos/SearchDto.java
@@ -0,0 +1,10 @@
+package com.deenn.demo.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchDto {
+    private String title;
+}
